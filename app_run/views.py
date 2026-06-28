0e49ff6fb8e9fd71807884494e22aafa54b9cc9f
Diller <makarenko.stepan@bk.ru>
2026-06-29 00:32:05 +0300

    add: create model Run

--- app_run/views.py
+++ app_run/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.conf import settings
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from django.conf import settings
+
 
 
 
