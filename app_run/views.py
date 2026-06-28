48d8467f3c8ca461b12a8380890bb2ad8654c6b9
Diller <makarenko.stepan@bk.ru>
2026-06-28 21:34:36 +0300

    add api_view company_details_view

--- app_run/views.py
+++ app_run/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+
+
+@api_view(['GET'])
+def company_details_view(request):
+    return Response(
+        {
+            "company_name": "Кардио и Ко",
+            "slogan": "До магазина — прогулка. Обратно — интервальная тренировка.",
+            "contacts": "г. Пульсово, ул. 180 BPM, д. 60",
+        },
+        status=status.HTTP_200_OK
+    )
